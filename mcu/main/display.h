b67c91d0d35d1994c0f40525277310262698a567
Stefan Dangl <tzuzuzj@gmx.at>
2025-05-03 16:57:37 +0200

    feat: integrate sensors

--- mcu/main/display.h
+++ mcu/main/display.h
@@ -6,5 +6,10 @@
   #include "pin_layout.h"
 #endif
 
+#define NUMBER_OF_DISPLAY_DIGITS 3
+#define MAX_DISPLAYED_VALUE 10000
+#define DIVIDER 10
+
 void sevsegInit();
 void displaySelectedSensor(int8_t sensor_id);
+void setDisplayedSensorValue(float sensorValue);
